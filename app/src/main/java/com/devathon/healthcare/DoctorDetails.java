98c7ecab04c37bd3df35ab31fb7a27b62ed3a52d
Ishan Gupta <gupta.ishan@hotmail.com>
2021-09-26 23:35:42 +0530

    Details Pages Backend Created

--- app/src/main/java/com/devathon/healthcare/DoctorDetails.java
+++ app/src/main/java/com/devathon/healthcare/DoctorDetails.java
@@ -2,10 +2,26 @@ package com.devathon.healthcare;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class DoctorDetails extends AppCompatActivity {
 
@@ -26,7 +42,71 @@ public class DoctorDetails extends AppCompatActivity {
         DocDelSpectextView = (TextView) findViewById(R.id.DocDelSpectextView);
         DocDelNumtextView = (TextView) findViewById(R.id.DocDelNumtextView);
         DocDelButton = (Button) findViewById(R.id.DocDelButton);
+        DocDelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Reload();
+            }
+        });
 
+
+    }
+    private void Reload() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.e(TAG, "onResponse: " + response);
+                        try {
+                            JSONArray jsonArray = new JSONArray(response);
+                            JSONObject jsonObject = jsonArray.getJSONObject(0);
+                            String code = jsonObject.getString("code");
+                            Log.e(TAG, "onResponse: json" + jsonObject);
+                            Log.e(TAG, "onResponse: " + jsonObject.getString("enroll"));
+
+
+                            if (code.equals("login fail")) {
+                                AlertDialog.Builder builder = null;
+                                builder.setTitle("ERROR");
+                                Log.e(TAG, "onResponse: 1" + code);
+                                builder.setMessage(jsonObject.getString("code"));
+                                builder.show();
+                            } else {
+                                Shareprefences.getInstance(getContext()).userlogin(jsonObject.getString("docNameEditText")
+                                        , jsonObject.getString("docIDEditText")
+                                        , jsonObject.getString("docAddEditText")
+                                        , jsonObject.getString("docSpecEditText")
+                                        , jsonObject.getString("docNumEditText"));
+
+                                DocDelNametextView.setText(Shareprefences.getInstance(getContext()).getname());
+                                DocDelAddetextView.setText(Shareprefences.getInstance(getContext()).getadd());
+                                DocDelSpectextView.setText(Shareprefences.getInstance(getContext()).getspec());
+                            }
+
+                        } catch (JSONException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(TAG, "onErrorResponse: ERROR" + error);
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> stringStringMap = new HashMap<String, String>();
+                stringStringMap.put("enroll", Shareprefences.getInstance(getContext()).getEnroll());
+                stringStringMap.put("pass", Shareprefences.getInstance(getContext()).getPass());
+                Log.e(TAG, "getParams: " + stringStringMap);
+                return stringStringMap;
+            }
+        };
+
+        MySet.getInstance(getContext()).addRequestQue(stringRequest);
 
     }
 }
